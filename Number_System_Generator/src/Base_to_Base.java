a53dab133334b1713907fa63d109643d9ef22f60
WhoDis-MaName <andrawilliams03.cs@gmail.com>
2023-07-04 00:28:35 -0500

    Update Base_to_Base.java

--- Number_System_Generator/src/Base_to_Base.java
+++ Number_System_Generator/src/Base_to_Base.java
@@ -11,8 +11,7 @@ import java.util.Scanner;
 public class Base_to_Base {
     // instance variables - replace the example below with your own
     public static char[] CHARCTERS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
-            'G',
-            'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!',
+            'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!',
             '@', '#', '$', '%', '^', '&', '*' };
 
     public static int toIntegerCheck(String testForType) {
